181a7168b71a2b80327136a30579b76744f323f7
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-11 09:59:49 +1000

    feat: added routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home.jsx'
+import AboutPage from './pages/About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={AboutPage} />
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+
+
+export default function AboutPage() {
+    return (
+        <div className="AboutPage">
+            <h1>About Page</h1>
+        </div>
+    )
+}
