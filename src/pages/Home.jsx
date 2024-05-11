69d8bf8f87bde4e5b138b50f42ba1c3d4aadc7da
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-11 11:53:28 +1000

    FEAT: ADDED NAV

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 export default function HomePage() {
     return (
         <div className="HomePage">
             <h1>Home Page</h1>
+            <Link to={"/about/services"}>
+                About
+            </Link>
         </div>
     )
 }
