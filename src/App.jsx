b9dab4a54764190fe8590778ca8cc1cb44aa1209
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-11 10:35:55 +1000

    feat: 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   
@@ -12,9 +13,19 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          {/*Absolute Paths */}
           <Route path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
           <Route path='/contact' element={<ContactPage />} />
+          <Route path='*' element={<PageNotFound />} />
+          {/* Relative Paths */}
+          {/* <Route path='/' element={<HomePage />}>
+            {/* <Route path='about' element={<AboutPage />} />
+            <Route path='about'>
+              <Route path='services' element={<AboutPage />} />
+            </Route>
+            <Route path='contact' element={<ContactPage />} />
+          </Route> */}
         </Routes>
       </BrowserRouter>
     </>
